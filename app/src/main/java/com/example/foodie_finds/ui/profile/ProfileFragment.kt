9158612587355997d6c388060122e3e509191131
Noam Ben Zeev <noambz100@gmail.com>
2024-06-08 17:01:31 +0300

    start to fix nav

--- app/src/main/java/com/example/foodie_finds/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/foodie_finds/ui/profile/ProfileFragment.kt
@@ -1,4 +1,4 @@
-package com.example.foodie_finds.ui.notifications
+package com.example.foodie_finds.ui.profile
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.foodie_finds.databinding.FragmentNotificationsBinding
 
-class NotificationsFragment : Fragment() {
+class ProfileFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
 
@@ -23,7 +23,7 @@ class NotificationsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+            ViewModelProvider(this).get(ProfileViewModel::class.java)
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
--- app/src/main/java/com/example/foodie_finds/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/foodie_finds/ui/profile/ProfileViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.foodie_finds.ui.notifications
+package com.example.foodie_finds.ui.profile
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class ProfileViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
